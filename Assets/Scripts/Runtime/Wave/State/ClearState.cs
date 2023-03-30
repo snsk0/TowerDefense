f70adaf291e7f1dfdd41eaa5af28ba6c8ef46fc5
shunsuke <snskngo03.sch.2021@gmail.com>
2023-03-31 00:07:39 +0900

    シーン切り替え

--- Assets/Scripts/Runtime/Wave/State/ClearState.cs
+++ Assets/Scripts/Runtime/Wave/State/ClearState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 using StateMachines;
 using StateMachines.BlackBoards;
 
@@ -6,6 +8,18 @@ namespace Runtime.Wave.State
 {
     public class ClearState : StateBase<WaveManager>
     {
-        public ClearState(WaveManager manager, IBlackBoard blackBoard) : base(manager, blackBoard){}
+        private ChangeScene change;
+        public ClearState(WaveManager manager, IBlackBoard blackBoard) : base(manager, blackBoard)
+        {
+            change = owner.GetComponent<ChangeScene>();
+        }
+
+
+        private float timer = 0;
+        public override void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > 5.0f) change.OnButton();
+        }
     }
 }
